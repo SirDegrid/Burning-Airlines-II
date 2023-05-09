875a1d3d8fc8c7096c45943458a3741fd508922a
Maggie Cameron <maggie.rose.cameron@gmail.com>
2023-05-09 17:56:38 +1000

    no decent changes, commitig to pull

--- burning-airlines-server/app/assets/config/cors.rb
+++ burning-airlines-server/app/assets/config/cors.rb
@@ -1,7 +1,9 @@
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      origins '*' # this needs to be an explicit list of URLs for Heroku
+      resource '*',
+        :headers => :any,
+        :methods => %i(get post put patch delete options head) # %i is array of symbols
     end
   end
