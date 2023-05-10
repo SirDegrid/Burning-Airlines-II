85319eeb7289e226801a9d6468f8a147f632ef07
Sambid Rana <sambid.rana1@gmail.com>
2023-05-10 22:08:37 +1000

    seed data and airplane_id update

--- burning-airlines-server/db/schema.rb
+++ burning-airlines-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_10_063149) do
+ActiveRecord::Schema.define(version: 2023_05_10_114820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2023_05_10_063149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "reservation_id"
+    t.integer "airplane_id"
   end
 
   create_table "reservations", force: :cascade do |t|
--- burning-airlines-server/db/seeds.rb
+++ burning-airlines-server/db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+u1 = User.create(:email => 'aa@ga.co', :password => 'chicken', :admin => true)
+u2 = User.create(:email => 'ab@ga.co', :password => 'chicken', :admin => false)
+u3 = User.create(:email => 'ac@ga.co', :password => 'chicken', :admin => false)
+u4 = User.create(:email => 'ad@ga.co', :password => 'chicken', :admin => false)
+
+puts "#{User.count} users"
+
+Airplane.destroy_all
+a1 = Airplane.create(:name => "Qantas", :row => 5, :column => "C")
+a2 = Airplane.create(:name => "Jetstar", :row => 12, :column => "X")
+a3 = Airplane.create(:name => "Tigerair", :row => 10, :column => "Y")
+puts "#{Airplane.count} airplanes"
+
+Flight.destroy_all
+f1 = Flight.create(:number => 414, :origin => "Sydney", :destination => "Melbourne", :flight_date => "2023-05-13")
+f2 = Flight.create(:number => 500, :origin => "Hobart", :destination => "Darwin", :flight_date => "2023-06-15")
+f3 = Flight.create(:number => 777, :origin => "Perth", :destination => "Brisbane", :flight_date => "2023-07-20")
+f4 = Flight.create(:number => 123, :origin => "Adelaide", :destination => "Cairns", :flight_date => "2023-08-10")
+f5 = Flight.create(:number => 691, :origin => "Darwin", :destination => "Sydney", :flight_date => "2023-06-16")
+f6 = Flight.create(:number => 666, :origin => "Brisbane", :destination => "Melbourne", :flight_date => "2023-07-20")
+puts "#{Flight.count} flights"
+
+#plane id and reservation id
+
+# associations
+#flight belongs to
+f1.airplane = a1
+f2.airplane = a1
+f3.airplane = a2
+f4.airplane = a2
+f5.airplane = a3
+f6.airplane = a3
+
+#airplane has many
+a1.flights << f1 << f2  #adding to the collection of a1
+a2.flights << f3 << f4
+a3.flights << f5 << f6
+
+#user has many reservations - reservations should come from/updated by  flight?
+
